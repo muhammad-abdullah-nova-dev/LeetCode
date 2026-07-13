27d18ff2f9d7cd06631cee4d8ed69960885cbfe1
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-07-13 12:38:55 +0500

    Time: 3 ms (37.76%), Space: 40.3 MB (45.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,13 @@ public:
         int n = nums.size();
         vector<int> answer(n, 1);
 
-        int product = 1;
-
-        // Prefix products
+        int prefix = 1;
         for (int i = 0; i < n; i++) {
-            answer[i] = product;
-            product *= nums[i];
+            answer[i] = prefix;
+            prefix *= nums[i];
         }
 
         int suffix = 1;
-
-        // Suffix products
         for (int i = n - 1; i >= 0; i--) {
             answer[i] *= suffix;
             suffix *= nums[i];
