c1d3b362437ef82f6d9f761ba876f34368d9eaa7
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-07-31 09:05:05 +0500

    Time: 9 ms (94.22%), Space: 48.2 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+
+        int[] freq = new int[26];
+
+        for(char c : word.toCharArray())
+            freq[c-'a']++;
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+        int rank = 0;
+
+        for(int i=25;i>=0;i--)
+        {
+            if(freq[i]==0)
+                break;
+
+            ans += freq[i] * ((rank/8)+1);
+            rank++;
+        }
+
+        return ans;
+    }
+}
