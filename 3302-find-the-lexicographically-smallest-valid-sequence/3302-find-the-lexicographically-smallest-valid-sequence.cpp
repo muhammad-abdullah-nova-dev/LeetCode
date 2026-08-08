1ae1901506b27711012297beb2686805b87d99b1
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-08-08 22:42:02 +0500

    Time: 1457 ms (5.32%), Space: 567.6 MB (6.38%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -4,19 +4,23 @@ public:
         int n = word1.size();
         int m = word2.size();
 
+        // nxtIdx[i][c] = first index >= i
+        // where character c occurs in word1
         vector<vector<int>> nxtIdx(n + 1, vector<int>(26, -1));
 
-        for(int i = n - 1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--) {
             nxtIdx[i] = nxtIdx[i + 1];
             nxtIdx[i][word1[i] - 'a'] = i;
         }
 
+        // suff[j] = position where word2[j] can be matched
+        // when matching word2 from right to left.
         vector<int> suff(m, -1);
 
         int k = m - 1;
 
-        for(int i = n - 1; i >= 0 && k >= 0; i--){
-            if(word1[i] == word2[k]){
+        for (int i = n - 1; i >= 0 && k >= 0; i--) {
+            if (word1[i] == word2[k]) {
                 suff[k] = i;
                 k--;
             }
@@ -26,42 +30,69 @@ public:
 
         int i = 0;
         int j = 0;
-        bool Used = false;
 
-        while(i < n && j < m){
+        // Have we already used our one allowed mismatch?
+        bool used = false;
+
+        while (i < n && j < m) {
+
+            // Earliest occurrence of word2[j]
+            // from position i onward.
             int idx = nxtIdx[i][word2[j] - 'a'];
 
-            if(Used == true){
-                if(idx == -1)
+            // ----------------------------------------
+            // Mismatch has already been used
+            // ----------------------------------------
+            if (used) {
+
+                if (idx == -1)
                     return {};
 
-                if(j < m - 1 && suff[j + 1] <= idx)
+                // We need enough room for the rest.
+                if (j < m - 1 && suff[j + 1] <= idx)
                     return {};
 
                 ans.push_back(idx);
+
                 i = idx + 1;
                 j++;
             }
-            else{
-                if(word1[i] == word2[j]){
+
+            // ----------------------------------------
+            // Mismatch is still available
+            // ----------------------------------------
+            else {
+
+                // Exact match at current position.
+                if (word1[i] == word2[j]) {
+
                     ans.push_back(i);
                     i++;
                     j++;
                 }
-                else if(j == m - 1 ||
-                        (suff[j + 1] != -1 && i < suff[j + 1])){
-                    Used = true;
+
+                // Use our one mismatch here.
+                else if (
+                    j == m - 1 ||
+                    (suff[j + 1] != -1 && i < suff[j + 1])
+                ) {
+
+                    used = true;
+
                     ans.push_back(i);
+
                     i++;
                     j++;
                 }
-                else{
+
+                // Current index cannot be used.
+                else {
                     i++;
                 }
             }
         }
 
-        if(ans.size() == m)
+        if (ans.size() == m)
             return ans;
 
         return {};
