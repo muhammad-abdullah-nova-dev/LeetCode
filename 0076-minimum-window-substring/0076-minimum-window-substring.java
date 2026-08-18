b14bf2f353bf3f2bd6b5a1772d49c8a3827b3917
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-08-18 22:05:08 +0500

    Time: 23 ms (24.03%), Space: 47.7 MB (23.83%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) return "";
+
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int formed = 0;
+        int required = need.size();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (need.containsKey(c) && 
+                window.get(c).equals(need.get(c))) {
+                formed++;
+            }
+
+            while (formed == required) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) &&
+                    window.get(leftChar) < need.get(leftChar)) {
+                    formed--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
