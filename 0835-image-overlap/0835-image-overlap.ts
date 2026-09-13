86d330a4c4dbd8524fc8114492b7991d4c6afc37
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-09-13 09:51:37 +0500

    Time: 20 ms (100%), Space: 63.8 MB (40%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.ts
@@ -0,0 +1,20 @@
+function largestOverlap(img1: number[][], img2: number[][]): number {
+    const n = img1.length;
+    const A: number[][] = [], B: number[][] = [];
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (img1[i][j] === 1) A.push([i, j]);
+            if (img2[i][j] === 1) B.push([i, j]);
+        }
+    }
+    const cnt: number[][] = Array.from({length: 2 * n}, () => Array(2 * n).fill(0));
+    let best = 0;
+    for (const a of A) {
+        for (const b of B) {
+            const dx = b[0] - a[0] + n;
+            const dy = b[1] - a[1] + n;
+            best = Math.max(best, ++cnt[dx][dy]);
+        }
+    }
+    return best;
+}
