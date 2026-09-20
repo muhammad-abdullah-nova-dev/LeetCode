a726213551503b988f913ea77d148af01514ca53
Muhammad Abdullah <m.abdullah.nova7@gmail.com>
2026-09-20 08:01:48 +0500

    Time: 2 ms (40%), Space: 57.7 MB (15.29%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.js
@@ -0,0 +1,7 @@
+var reverseDegree = function (s) {
+    let ans = 0;
+    for (let i = 1; i <= s.length; i++) {
+        ans += (26 - (s.charCodeAt(i - 1) - 'a'.charCodeAt(0))) * i;
+    }
+    return ans;
+};
